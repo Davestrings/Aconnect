e464d45b72153e85dd1d7353d6440a35d7cd5a06
Fatunbi David Oluwakayode <fatunbidavidkayode@gmail.com>
2020-08-20 13:39:28 +0100

    partially completed profiles model

--- profiles/models.py
+++ profiles/models.py
@@ -10,8 +10,11 @@ class Profile(models.Model):
     first_name = models.CharField(max_length=200, blank=True)
     last_name = models.CharField(max_length=200, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(default="no bio...", max_length=300)
-    email = models.EmailField(max_length=200, blank=True)
+    bio = models.TextField(default="no bio...", max_length=300, verbose_name='bio')
+    DOB = models.DateField(verbose_name='Date of Birth', blank=True, null=True)
+    email = models.EmailField(max_length=200, blank=True, verbose_name='email')
+    address = models.CharField(max_length=300, blank=True, verbose_name='address')
+    phone_number = models.CharField(max_length=15, blank=True, verbose_name='phone number')
     country = models.CharField(max_length=200, blank=True)
     avatar = models.ImageField(default='avatar.png', upload_to='avatars/')
     friends = models.ManyToManyField(User, blank=True, related_name='friends')
@@ -19,14 +22,20 @@ class Profile(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    def get_friends(self):
+        return self.friends.all()
+
+    def get_friends_number(self):
+        return self.friends.all().count()
+
     def __str__(self):
-        return f"{self.user.username}-{self.created.strftime('%d-%n-%Y')}"
+        return f"{self.user.username}-{self.created.strftime('%d-%m-%Y')}"
 
     def save(self, *args, **kwargs):
         ex = False
         if self.first_name and self.last_name:
             to_slug = slugify(str(self.first_name) + " " + str(self.last_name))
-            ex = Profile.objects.filters(slug=to_slug).exists()
+            ex = Profile.objects.filter(slug=to_slug).exists()
             while ex:
                 to_slug = slugify(to_slug + " " + str(get_random_code()))
                 ex = Profile.objects.filter(slug=to_slug).exists()
