ab453effad711e203b312402842ad75351a3e737
Cynthia Odiase <cynthiaodiase@gmail.com>
2020-08-21 06:48:01 +0100

    Posts, Likes and Comment features

--- chitchat/urls.py
+++ chitchat/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('profiles/', include('profiles.urls', namespace='profiles')),
+    path('posts/', include('posts.urls', namespace='posts')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
