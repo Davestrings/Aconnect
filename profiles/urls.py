bff0be4fec287b8b6ee7b2b2f32cead38597178a
Fatunbi David Oluwakayode <fatunbidavidkayode@gmail.com>
2020-08-31 11:22:55 +0100

    profile updates2

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import my_profile_view, invite_received_view, profile_list_view, profiles_i_can_invite
+from .views import my_profile_view, invite_received_view, profile_list_view, profiles_i_can_invite, ProfileListView
 
 app_name = 'profiles'
 
 urlpatterns = [
     path('myprofile/', my_profile_view, name='my_profile_view'),
     path('invites/', invite_received_view, name='invites'),
-    path('allprofiles/', profile_list_view, name='allprofiles'),
+    path('allprofiles/', ProfileListView.as_view(), name='all_profiles'),
     path('invite_friends/', profiles_i_can_invite, name='invite_friends'),
 ]
