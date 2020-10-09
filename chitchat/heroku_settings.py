e420e24bdb71014600519d0c29aecf17023b32a7
Fatunbi David Oluwakayode <fatunbidavidkayode@gmail.com>
2020-10-09 13:52:35 +0100

    new commit

--- chitchat/heroku_settings.py
+++ chitchat/heroku_settings.py
@@ -13,7 +13,7 @@ DATABASES = {
 # if environment variable present, it must be Heroku
 if 'DATABASE_URL' in os.environ:
     # replace default with parsed value
-    DATABASES['default'] = dj_database_url.config(ssl_require=True)
+    DATABASES['default'] = dj_database_url.config()
 
     import django_heroku
     django_heroku.settings(locals())
